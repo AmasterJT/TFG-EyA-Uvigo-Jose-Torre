2554f93cd5195d5fdd6bccddfbcdbda30bb28763
AmasterJT <amasterjt@gmail.com>
2024-11-22 13:58:18 +0100

    restart java proyect

--- TFG-almacen/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module uvigo.tfgalmacen {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens uvigo.tfgalmacen to javafx.fxml;
-    exports uvigo.tfgalmacen;
-}
