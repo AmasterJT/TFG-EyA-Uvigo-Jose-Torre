7636830f141c9ca2d6f91aab44644246cada0b8b
AmasterJT <amasterjt@gmail.com>
2025-06-21 19:40:15 +0200

    creamos el controller del itemPedido

---
+++ TFG-almacen/src/main/java/uvigo/tfgalmacen/controllers/ItemPedidoController.java
@@ -0,0 +1,21 @@
+package uvigo.tfgalmacen.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
+public class ItemPedidoController {
+
+    @FXML
+    private CheckBox check_select_item_pedido;
+
+    @FXML
+    private Label estado_pedido_label;
+
+    @FXML
+    private Label id_pedido_label;
+
+    @FXML
+    private Label nombre_cliente_label;
+
+}
