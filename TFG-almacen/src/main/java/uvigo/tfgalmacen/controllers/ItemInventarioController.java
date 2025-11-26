26617144adcad7399f4855f593406a8613077e8e
AmasterJT <amasterjt@gmail.com>
2025-11-26 23:26:47 +0100

    ahora podemos copìar el id de los palets en el inventario

--- TFG-almacen/src/main/java/uvigo/tfgalmacen/controllers/ItemInventarioController.java
+++ TFG-almacen/src/main/java/uvigo/tfgalmacen/controllers/ItemInventarioController.java
@@ -1,11 +1,18 @@
 package uvigo.tfgalmacen.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import uvigo.tfgalmacen.almacenManagement.Palet;
 
-public class ItemInventarioController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static uvigo.tfgalmacen.utils.ClipboardUtils.copyLabelText;
+
+public class ItemInventarioController implements Initializable {
 
     @FXML
     private Label baldaLabel;
@@ -32,6 +39,9 @@ public class ItemInventarioController {
     @FXML
     private AnchorPane backgroundAnchorPane;
 
+    @FXML
+    private Button copy_idpalet_btn;
+
     public void setData(Palet palet) {
         // Código para llenar los labels o nodos con la información del palet
 
@@ -48,6 +58,12 @@ public class ItemInventarioController {
         delanteLabel.setText(del);
 
         String colorHEX = palet.getProducto().getTipo().applyPastelFilter(palet.getProducto().getTipo().colorHEX);
-        backgroundAnchorPane.setStyle("-fx-background-color: " +  colorHEX);
+        backgroundAnchorPane.setStyle("-fx-background-color: " + colorHEX);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        copy_idpalet_btn.setOnMouseClicked(_ -> copyLabelText(copy_idpalet_btn, idPaletLabel));
+
     }
 }
